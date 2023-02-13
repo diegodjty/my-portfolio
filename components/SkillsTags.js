0176c293aec5f4ca8a6cbbd4addbb889eabdac78
Diego Taveras <diegotaveras2@outlook.com>
2023-02-13 15:18:48 -0500

    change skillTags font-size and make it uppercase

--- components/SkillsTags.js
+++ components/SkillsTags.js
@@ -1,10 +1,12 @@
-function SkillsTags({skill}) {
-
-    return (
-        <div className={`font-bold bg-blue-medium px-3 py-[2px] border border-black rounded-md `} style={{color:`${skill.color}`}}>
-            #{skill.title}
-        </div>
-    )
+function SkillsTags({ skill }) {
+  return (
+    <div
+      className={`font-bold bg-blue-medium px-3 py-[2px] border border-black rounded-md uppercase text-sm `}
+      style={{ color: `${skill.color}` }}
+    >
+      #{skill.title}
+    </div>
+  );
 }
 
-export default SkillsTags
+export default SkillsTags;
