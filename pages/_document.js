a0f124669d2dc7fc1fb207b32997db1c41cfe3c9
Diego <diegotaveras2@outlook.com>
2023-04-08 02:32:35 -0400

    nextjs _document fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,21 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-<body>
-  <Main />
-  <NextScript />
-  <noscript
-    dangerouslySetInnerHTML={{
-      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2KD8WT"
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2KD8WT"
       height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-    }}
-  />
-</body>;
+            }}
+          />
+        </body>
+      </Html>
+    );
+  }
+}
