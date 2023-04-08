e073766a6553fce8e90e43d64011394d6e8d3e75
Diego <diegotaveras2@outlook.com>
2023-04-08 02:11:39 -0400

    add google tag manager

---
+++ pages/_document.js
@@ -0,0 +1,12 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+<body>
+  <Main />
+  <NextScript />
+  <noscript
+    dangerouslySetInnerHTML={{
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2KD8WT"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+    }}
+  />
+</body>;
